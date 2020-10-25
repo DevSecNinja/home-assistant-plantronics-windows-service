9a9c39084a701a1feaed720046195d58b4d83092
Jean-Paul van Ravensberg <14926452+Cloudenius@users.noreply.github.com>
2020-10-25 22:47:27 +0100

    Add sensor integration with Home Assistant

---
+++ homeassistant.py
@@ -0,0 +1,54 @@
+import json
+from plantronics import Spokes
+from requests import post
+
+
+class HomeAssistant:
+    def __init__(self):
+        with open("./secrets.json") as secrets_file:
+            secrets_data = json.load(secrets_file)
+
+        self.host = secrets_data["homeAssistantUrl"]
+        self.accessToken = secrets_data["homeAssistantAccessToken"]
+
+
+class Sensor(HomeAssistant):
+    def __init__(self):
+        s = Spokes()
+        self.name = s.get_device_info()["Result"]["ProductName"]
+        self.friendlyName = self.name + " Adapter"
+        self.icon = "mdi:headset"
+
+    def set_sensor(self, state):
+        self.name = str(Sensor().name).replace(" ", "_")
+        self.host = HomeAssistant().host + "/api/states/" + "sensor." + self.name
+        self.accessToken = HomeAssistant().accessToken
+        self.headers = {
+            "Authorization": self.accessToken,
+            "content-type": "application/json",
+        }
+
+        response = post(
+            url=self.host,
+            headers=self.headers,
+            data=json.dumps(
+                {
+                    "state": state,
+                    "attributes": {
+                        "friendly_name": Sensor().friendlyName,
+                        "icon": Sensor().icon,
+                    },
+                }
+            ),
+        )
+
+        if response.status_code == 200:
+            print(response.text)
+
+            responseJson = json.loads(response)
+            self.sensorInfo = responseJson
+            return self.sensorInfo
+        else:
+            raise AssertionError(
+                "Response should have status code 200, but was:", response.status_code
+            )
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from plantronics import Spokes
+from homeassistant import Sensor
+from requests import post
+
+import time
+import logging
 
 s = Spokes()
-state = s.get_callmanager_state()
+sen = Sensor()
+lastState = s.get_callmanager_state()["Result"]["HasActiveCall"]
 
-# Output call manager state
-print(state["Result"]["HasActiveCall"])
+# Continuously check Plantronics Call Manager State
+while True:
+    newState = s.get_callmanager_state()["Result"]["HasActiveCall"]
+    logging.debug(
+        "Comparing last activate call state:",
+        lastState,
+        "with new call state:",
+        newState,
+    )
 
-# TODO: Send Call Manager state to Home Assistant
+    if lastState != newState:
+        print("Updating Home Assistant API with new call state:", newState)
+
+        if newState is True:
+            sen.set_sensor("on")
+        elif newState is False:
+            sen.set_sensor("off")
+        else:
+            raise AssertionError("State should be either True or False")
+
+    lastState = newState
+    time.sleep(0.5)
 
 # TODO: Create Windows Service
